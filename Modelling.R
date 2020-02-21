56057229d3a1f0263942fe61b2c9f338d394499d
quickcoffee <31934316+quickcoffee@users.noreply.github.com>
2020-02-21 15:45:04 +0100

    adding evaluation

--- Modelling.R
+++ Modelling.R
@@ -36,7 +36,7 @@ train_folds %>%
 #define control_grid for all models
 cntrl <-  control_grid(verbose = TRUE, save_pred = FALSE, allow_par = TRUE)
 
-##### LM ######
+# LM ----------------------------------------------------------------------
 #setup recipe with normalization of numerical variables
 jannowitz_rec_lm <- recipe(jannowitz_n ~ ., data = train) %>%
   step_rm(date) %>% 
@@ -75,8 +75,14 @@ initial_lm %>%
 initial_lm %>% 
   show_best(metric = "rmse", maximize = F)
 
+autoplot(initial_lm)
 
+saveRDS(initial_lm, file = "model_backup/initial_lm.rds")
 
+lm_fitted <- initial_lm %>% 
+  select_best(metric = "rmse", maximize = FALSE) %>% 
+  finalize_workflow(x = lm_wflow) %>% 
+  fit(data=train)
 
 # MARS --------------------------------------------------------------------
 #setup recipe with normalization of numerical variables
@@ -124,7 +130,14 @@ initial_mars %>%
 initial_mars %>% 
   show_best(metric = "rmse", maximize = FALSE)
 
-saveRDS(initial_mars, file = "initial_mars.rds")
+autoplot(initial_mars)
+
+saveRDS(initial_mars, file = "model_backup/initial_mars.rds")
+
+mars_fitted <- initial_mars %>% 
+  select_best(metric = "rmse", maximize = FALSE) %>% 
+  finalize_workflow(x = mars_wflow) %>% 
+  fit(data=train)
 
 # KNN ---------------------------------------------------------------------
 #knn model - baseline
@@ -143,16 +156,24 @@ knn_grid <-  grid_max_entropy(neighbors(range = c(1,20)),
 
 #run model with resampling
 set.seed(456321)
-initial_knn <- tune_grid(knn_wflow, resamples = train_folds, control = cntrl, grid = )
+initial_knn <- tune_grid(knn_wflow, resamples = train_folds, control = cntrl, grid = knn_grid)
 
 #show performance across resamples
 initial_knn %>% 
-  collect_metrics(summarize = F)
+  collect_metrics(summarize = F) %>% 
+  filter(.metric == "rmse")
 #show summarized performance across resamples
 initial_knn %>% 
   collect_metrics(summarize = T)
 
-saveRDS(initial_knn, file = "initial_knn.rds")
+autoplot(initial_knn)
+
+saveRDS(initial_knn, file = "model_backup/initial_knn.rds")
+
+knn_fitted <- initial_knn %>% 
+  select_best(metric = "rmse", maximize = FALSE) %>% 
+  finalize_workflow(x = knn_wflow) %>% 
+  fit(data=train)
 
 # random forest -----------------------------------------------------------
 
@@ -186,12 +207,12 @@ autoplot(initial_rf)
 initial_rf %>% 
   collect_metrics(summarize = F)
 
-saveRDS(initial_rf, file = "initial_rf.rds")
+saveRDS(initial_rf, file = "model_backup/initial_rf.rds")
 
-rf_low_rmse <- select_best(initial_rf, metric = "rmse", maximize = FALSE)
-rf_low_rmse
-rf_final_wflow <- finalize_workflow(rf_wflow, rf_low_rmse)
-rf_fitted <- fit(rf_final_wflow, data = train)
+rf_fitted <- initial_rf %>% 
+  select_best(metric = "rmse", maximize = FALSE) %>% 
+  finalize_workflow(x = rf_wflow) %>% 
+  fit(data=train)
 
 rf_test <- rf_fitted %>% 
   predict(new_data = test) %>% 
@@ -206,4 +227,53 @@ rf_test %>%
                           '<br>Prediction: ', round(.pred),
                           '<br>Date: ', as.Date(day), " ", hour, ":00")))+
   geom_point()+
-    labs(title = paste("Random Forest on test set (01-12-2019 to 31-12-2019), RMSE: ", round(rmse(data= rf_test, truth = truth, estimate = .pred)[3], 2))), tooltip = c("text"))
+    labs(title = paste("Random Forest on test set (02-12-2019 to 31-12-2019), RMSE: ", round(rmse(data= rf_test, truth = truth, estimate = .pred)[3], 2))), tooltip = c("text"))
+
+
+
+
+# evaluation --------------------------------------------------------------
+
+rmse_best_models <- rbind(show_best(initial_lm, metric = "rmse", maximize = F, n = 1) %>% select(mean) %>% mutate(model = "lm"),
+      show_best(initial_mars, metric = "rmse", maximize = F, n = 1)%>% select(mean) %>% mutate(model = "MARS"),
+      show_best(initial_knn, metric = "rmse", maximize = F, n = 1)%>% select(mean) %>% mutate(model = "KNN"),
+      show_best(initial_rf, metric = "rmse", maximize = F, n = 1)%>% select(mean) %>% mutate(model = "rF"))
+rmse_best_models %>% 
+  ggplot(aes(x=model, y=mean, fill=model))+
+  geom_col()+
+  labs(y="Mean RMSE for best candidate model across resamples",
+       x="Model",
+       fill="Model")
+
+test_performace <- tibble(truth = test$jannowitz_n) %>% 
+  mutate(lm = predict(lm_fitted, new_data = test)$.pred,
+         mars = predict(mars_fitted, new_data = test)$.pred,
+         knn = predict(knn_fitted, new_data = test)$.pred,
+         rf = predict(rf_fitted, new_data = test)$.pred,
+         week = as.factor(isoweek(test$date)),
+         day = date(test$date),
+         hour = hour(test$date))
+
+plot_residuals <- function(model, test_tibble=test_performace){
+  mod_str <- deparse(substitute(model))
+  mod_var <- enquo(model)
+  ggplotly(
+    test_tibble %>% 
+      ggplot(aes(x=truth, y=!!mod_var, color = week,
+                 text = paste('Truth ', truth,
+                              '<br>Prediction: ', round(!!mod_var),
+                              '<br>Date: ', as.Date(day), " ", hour, ":00")))+
+      geom_point()+
+      labs(title = paste(mod_str,"on test set (02-12-2019 to 31-12-2019), RMSE:",
+                         round(rmse(data= test_tibble, truth = truth,
+                                    estimate = !!mod_var)[3], 2)),
+           y="Predictions",
+           x="True value",
+           color = "Week")+
+      theme_light(),
+    tooltip = c("text"))
+}
+plot_residuals(lm)
+plot_residuals(mars)
+plot_residuals(knn)
+plot_residuals(rf)
